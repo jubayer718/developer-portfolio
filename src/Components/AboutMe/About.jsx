8d7a9003114336736f2091f98d90b3b368205fae
jubayer718 <jubayerahmed2462@gmail.com>
2025-02-07 09:10:12 +0600

    about me section done

--- src/Components/AboutMe/About.jsx
+++ src/Components/AboutMe/About.jsx
@@ -3,7 +3,18 @@
 const About = () => {
   return (
     <div className="mt-20">
-      about
+      <div className="mx-7">
+        <h3 className="text-3xl font-bold text-center my-5 underline">About Me</h3>
+        <p>
+          Hello! I'm Jubayer Ahmed, a passionate programmer with a deep love for problem-solving and building innovative solutions. My journey into the world of coding started as a kid tinkering with HTML and CSS. Over the years, I've honed my skills in react JS, and I thrive on tackling complex challenges that push the boundaries of what's possible.
+
+          I enjoy working on full-stack web applications, and I find immense satisfaction in writing clean, efficient, and scalable code. Whether it's collaborating on a team project or diving into a solo endeavor, I always bring a problem-solving mindset and a keen eye for detail.
+
+          Beyond programming, I'm an avid tree plantation, and I love exploring new creative outlets that inspire my work. When I'm not coding, you can often find me in Garden.
+
+          I believe that technology is as much about creativity as it is about logic, and I strive to bring both elements into everything I do. If you're interested in collaborating or just geeking out over tech, feel free to connect with me!
+        </p>
+      </div>
     </div>
   );
 };
